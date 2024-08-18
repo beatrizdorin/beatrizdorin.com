5c1da93d16b7388a218e0f9d6b76add0aa678438
Beatriz Dorin <beatrizdorin@gmail.com>
2024-08-18 18:01:30 -0300

    envio do projeto

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Beatriz Dorin</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color:#555;
+                text-align: center;
+                color:azure;
+                padding: 50px;
+            }
+            .paragrafo-pequeno { 
+                font-size: 26px; 
+            }
+            .paragrafo-grande { 
+                font-size: 30px; 
+            }
+
+            .emoji {
+            font-size: 40px; 
+            }
+
+            h1 {
+                font-size: 3.5em; 
+                color:blanchedalmond; 
+                text-align: center;
+            }
+            
+            .social-icons {
+                display: flex; 
+                justify-content: center; 
+                gap: 30px; 
+                margin-top: 40px; 
+            }
+
+            .social-icons a {
+                color:aliceblue; 
+                font-size: 40px; 
+                text-decoration: none; 
+                transition: color 0.3s;
+            }
+
+            .social-icons a:hover {
+                color:royalblue; 
+            }
+
+            .link-repositorio {
+            font-size: 16px; 
+            text-decoration: none;
+            color: dodgerblue;
+            }
+
+            .link-repositorio:hover {
+            text-decoration: underline; 
+
+            }
+            .estrela {
+            font-size: 20px; 
+            color: #f39c12; 
+            }
+            </style>
+    </head>
+    <body>
+       
+        <h1>Olá <span class="emoji">👋</span> , sou a Beatriz Dorin</h1>
+
+        <p class="paragrafo-pequeno">
+            Dev iniciante e apaixonada por tecnologia.<br><br><br><br>
+        </p>
+        <p class="paragrafo-grande">    
+            Redes:
+        </p>
+
+        <div class="social-icons">
+            <a href="https://www.instagram.com/beatrizzdorin" target="_blank">
+                <i class="fab fa-instagram"></i> 
+            </a>
+            
+            <a href="https://x.com/beatrizzdorin" target="_blank">
+                <i class="fab fa-twitter"></i> 
+            </a>
+            
+            <a href="https://www.linkedin.com/in/beatrizdorin/" target="_blank">
+                <i class="fab fa-linkedin"></i> 
+            </a>
+           
+            <a href="https://github.com/beatrizdorin" target="_blank">
+                <i class="fab fa-github"></i> 
+            </a>
+        </div>
+
+        <p><br><br><br><br><br><br><br> Deixe uma estrelinha no repositório deste projeto no GitHub!<br></p>
+        <p>É só clicar aqui: 
+            <a href="https://github.com/beatrizdorin/beatrizdorin.com" class="link-repositorio" target="_blank">
+            REPOSITÓRIO <span class="estrela">&#9733;</span>
+            </a>
+         </p>
+
+    </body>
+</html>
